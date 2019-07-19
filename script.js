821fba4a1cb06197d818c02e5710b8ea9dee7bf1
enestatli <eenestatli@gmail.com>
2019-07-19 12:20:22 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  displayContent()
+})
+
+const displayContent = () => {
+  $(".loader").addClass("loading")
+  $.ajax({
+  type:"GET",
+  url:"https://test.cors.workers.dev/?https://www.bloomberght.com/piyasa/intradaydata/dolar",
+  dataType: 'json',
+  success: function(data) {
+    var results = $(data.SeriesData.splice(data.SeriesData.length-10).reverse())
+      $(results).each(function(index,item) {
+        $('.table tbody').append(
+          '<tr><td>'+ (index + 1) +
+          '</td><td>' + item[1] +
+          '</td><td>' + new Date(item[0]).toLocaleString() +
+          '</td></tr>'
+        )
+      })
+    },
+  });
+  setTimeout(function(){
+    $('.loader').removeClass("loading")
+  },1500)
+}
+
+$('.btn').click(function() {
+  $('tbody').empty()
+  displayContent()
+})
